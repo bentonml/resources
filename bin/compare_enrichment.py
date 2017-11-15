e04d3211080963c308c9bfff3da1bd025564018d
bentonml <mary.l.benton@vanderbilt.edu>
2017-11-15 13:00:54 -0600

    updated to empirical p from t-test

--- bin/compare_enrichment.py
+++ bin/compare_enrichment.py
@@ -3,14 +3,13 @@
 # Mary Lauren Benton, 2017
 #
 # Calculate empirical p-value to compare enrichment between simulations
-# Assumes comparison of region from the shuffled files; performs t-test
+# Assumes comparison of region from the shuffled files
 #
 
 import sys
 import argparse
 import datetime
 import numpy as np
-from scipy import stats
 
 ###
 #   arguments
@@ -40,8 +39,18 @@ def read_counts(filename):
     return obs, np.array(exp_list, dtype=int)
 
 
-def calculate_fc_dist(obs, exp_list):
+def calculate_fc_distribution(obs, exp_list):
     return np.array([(obs + 1.0) / (exp + 1.0) for exp in exp_list])
+
+
+def calculate_empirical_p(obs_fc_a, fc_list_a, obs_fc_b, fc_list_b):
+    delta_final = obs_fc_a - obs_fc_b
+    delta_dist  = [fc_list_a[i] - fc_list_b[i] for i in range(len(fc_list_a))]
+
+    # sum number of differences >= to observed difference
+    p_sum = sum(1 for delta in delta_dist if abs(delta) >= abs(delta_final))
+
+    return (p_sum + 1.0) / (len(delta_dist) + 1.0)
 
 
 ###
@@ -50,16 +59,21 @@ def calculate_fc_dist(obs, exp_list):
 def main(argv):
     # print header
     print('{:s} {:s}'.format(' '.join(sys.argv), str(datetime.datetime.now())[:20]))
-    print('TestStat\tp-value')
+    print('p-value')
 
     obs_a, exp_list_a = read_counts(COUNTS_A_FILENAME)
     obs_b, exp_list_b = read_counts(COUNTS_B_FILENAME)
 
-    fc_list_a = calculate_fc_dist(obs_a, exp_list_a)
-    fc_list_b = calculate_fc_dist(obs_b, exp_list_b)
+    fc_list_a = calculate_fc_distribution(obs_a, exp_list_a)
+    fc_list_b = calculate_fc_distribution(obs_b, exp_list_b)
 
-    result = stats.ttest_ind(fc_list_a, fc_list_b, equal_var=False)
-    print('{:.3f}\t{:.3e}'.format(result.statistic, result.pvalue))
+    final_obs_a = (obs_a + 1.0) / (np.mean(exp_list_a) + 1.0)
+    final_obs_b = (obs_b + 1.0) / (np.mean(exp_list_b) + 1.0)
+
+    p_val = calculate_empirical_p(final_obs_a, fc_list_a, final_obs_b, fc_list_b)
+
+    # print result
+    print('{:.3f}'.format(p_val))
 
 
 if __name__ == "__main__":
