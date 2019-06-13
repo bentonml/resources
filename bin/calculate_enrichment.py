22da313e9c5a7e28a8ae848b13b1d1c2a0c41acd
bentonml <mary.l.benton@vanderbilt.edu>
2019-06-13 11:26:07 -0500

    add support for dm3

--- bin/calculate_enrichment.py
+++ bin/calculate_enrichment.py
@@ -6,11 +6,15 @@
 #           | 2018.10.11
 #           | 2018.10.29
 #           | 2019.02.01
+#           | 2019.04.08
+#           | 2019.06.10
 #
 #   depends on:
 #       BEDtools v2.23.0-20 via pybedtools
 #       /dors/capra_lab/data/dna/[species]/[species]/[species]_trim.chrom.sizes
+#       /dors/capra_lab/data/dna/[species]/[species]_chrom-sizes.bed
 #       /dors/capra_lab/users/bentonml/data/dna/[species]/[species]_blacklist_gap.bed
+#       /dors/capra_lab/data/dna/[species]/[species]-blacklist.bed
 ###
 
 import os
@@ -34,7 +38,7 @@ arg_parser.add_argument("region_file_2", help='bed file 2 (not shuffled)')
 arg_parser.add_argument("-i", "--iters", type=int, default=100,
                         help='number of simulation iterations; default=100')
 
-arg_parser.add_argument("-s", "--species", type=str, default='hg19', choices=['hg19', 'hg38', 'mm10'],
+arg_parser.add_argument("-s", "--species", type=str, default='hg19', choices=['hg19', 'hg38', 'mm10', 'dm3'],
                         help='species and assembly; default=hg19')
 
 arg_parser.add_argument("-n", "--num_threads", type=int,
@@ -46,6 +50,9 @@ arg_parser.add_argument("--print_counts_to", type=str, default=None,
 arg_parser.add_argument("--elem_wise", action='store_true', default=False,
                         help='perform element-wise overlaps; default=False')
 
+arg_parser.add_argument("--by_hap_block", action='store_true', default=False,
+                        help='perform haplotype-block overlaps; default=False')
+
 args = arg_parser.parse_args()
 
 # save parameters
@@ -55,6 +62,7 @@ COUNT_FILENAME = args.print_counts_to
 ITERATIONS = args.iters
 SPECIES = args.species
 ELEMENT = args.elem_wise
+HAPBLOCK= args.by_hap_block
 
 # calculate the number of threads
 if args.num_threads:
@@ -69,35 +77,44 @@ else:
 def loadConstants(species):  # note chrom.sizes not used in current implementation | 2018.10.29
     return {'hg19': ("/dors/capra_lab/users/bentonml/data/dna/hg19/hg19_blacklist_gap.bed", "/dors/capra_lab/data/dna/human/hg19/hg19_trim.chrom.sizes"),
             'hg38': ("/dors/capra_lab/users/bentonml/data/dna/hg38/hg38_blacklist_gap.bed", "/dors/capra_lab/data/dna/human/hg38/hg38_trim.chrom.sizes"),
-            'mm10': ("/dors/capra_lab/users/bentonml/data/dna/mm10/mm10_blacklist_gap.bed", "/dors/capra_lab/data/dna/mouse/mm10/mm10_trim.chrom.sizes")
+            'mm10': ("/dors/capra_lab/users/bentonml/data/dna/mm10/mm10_blacklist_gap.bed", "/dors/capra_lab/data/dna/mouse/mm10/mm10_trim.chrom.sizes"),
+            'dm3' : ("/dors/capra_lab/data/dna/fly/dm3-blacklist.bed", "/dors/capra_lab/data/dna/fly/dm3_chrom-sizes.bed")
             }[species]
 
 
-def calculateObserved(annotation, test, elementwise):
+def calculateObserved(annotation, test, elementwise, hapblock):
     obs_sum = 0
 
     if elementwise:
         obs_sum = annotation.intersect(test, u=True).count()
     else:
         obs_intersect = annotation.intersect(test, wo=True)
-        for line in obs_intersect:
-            obs_sum += int(line[-1])
+
+        if hapblock:
+            obs_sum = len(set(x[-2] for x in obs_intersect))
+        else:
+            for line in obs_intersect:
+                obs_sum += int(line[-1])
 
     return obs_sum
 
 
-def calculateExpected(annotation, test, elementwise, species, iters):
+def calculateExpected(annotation, test, elementwise, hapblock, species, iters):
     BLACKLIST, CHROM_SZ = loadConstants(species)  # note CHROM_SZ not used
     exp_sum = 0
 
-    rand_file = annotation.shuffle(genome='hg19', excl=BLACKLIST, chrom=True, noOverlapping=True)
+    rand_file = annotation.shuffle(genome=species, excl=BLACKLIST, chrom=True, noOverlapping=True)
 
     if elementwise:
         exp_sum = rand_file.intersect(test, u=True).count()
     else:
         exp_intersect = rand_file.intersect(test, wo=True)
-        for line in exp_intersect:
-            exp_sum += int(line[-1])
+
+        if hapblock:
+            exp_sum = len(set(x[-2] for x in exp_intersect))
+        else:
+            for line in exp_intersect:
+                exp_sum += int(line[-1])
 
     return exp_sum
 
@@ -123,11 +140,11 @@ def main(argv):
     print('Observed\tExpected\tStdDev\tFoldChange\tp-value')
 
     # run initial intersection and save
-    obs_sum = calculateObserved(BedTool(ANNOTATION_FILENAME), BedTool(TEST_FILENAME), ELEMENT)
+    obs_sum = calculateObserved(BedTool(ANNOTATION_FILENAME), BedTool(TEST_FILENAME), ELEMENT, HAPBLOCK)
 
     # create pool and run simulations in parallel
     pool = Pool(num_threads)
-    partial_calcExp = partial(calculateExpected, BedTool(ANNOTATION_FILENAME), BedTool(TEST_FILENAME), ELEMENT, SPECIES)
+    partial_calcExp = partial(calculateExpected, BedTool(ANNOTATION_FILENAME), BedTool(TEST_FILENAME), ELEMENT, HAPBLOCK, SPECIES)
     exp_sum_list = pool.map(partial_calcExp, [i for i in range(ITERATIONS)])
 
     # wait for results to finish before calculating p-value
